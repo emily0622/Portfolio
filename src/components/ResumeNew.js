42187c4dab6c024cc4cacbcd31f1f3d337044d36
Emily Traynor <EJTraynor2001@Gmail.com>
2023-01-11 00:46:45 -0500

    changed port number

--- src/components/ResumeNew.js
+++ src/components/ResumeNew.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/Emily_Traynor_Resume_Website.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+// import React, { useState, useEffect } from "react";
+// import { Container, Row } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// import Particle from "../Particle";
+// import pdf from "../../Assets/Emily_Traynor_Resume_Website.pdf";
+// import { AiOutlineDownload } from "react-icons/ai";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 // const resumeLink =
 //   // "https://raw.githubusercontent.com/emily0622/Portfolio/master/src/Assets/Emily_Traynor_Resume.pdf";

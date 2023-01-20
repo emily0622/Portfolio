72483cfa4d28ab1f5c6cab377e259f402ecfa500
Emily Traynor <emily.traynor@mail.utoronto.ca>
2023-01-20 17:35:02 -0500

    changed react/scripts, react/dom and react version

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -5,8 +5,6 @@ import myImg from "../../Assets/diff_avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 

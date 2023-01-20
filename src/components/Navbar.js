61d6864c42044cff1cbcd748d437f979db9e680a
Emily Traynor <emily.traynor@mail.utoronto.ca>
2023-01-20 17:23:44 -0500

    just a home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,11 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo_me.png";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
 // import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
 } from "react-icons/ai";
 
 import { CgFileDocument } from "react-icons/cg";
